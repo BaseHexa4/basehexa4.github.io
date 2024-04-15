bfab05cd16912b1afe13a6a6915feab71220f02b
BaseHexa4 <75879820+BaseHexa4@users.noreply.github.com>
2024-04-16 00:23:58 +0800

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,50 @@
+(() => {
+    const list = document.getElementById('event-list');
+    const items = list.querySelectorAll('li');
+    if (items.length <= 3) {
+        list.style.justifyContent = 'center';
+    } else {
+        list.style.justifyContent = 'flex-start';
+    }
+
+
+    let startX = 0;
+    let scrollLeft = 0;
+    let isDown = false;
+    
+    // Mouse events
+    list.addEventListener('mousedown', startDrag);
+    list.addEventListener('mouseleave', endDrag);
+    list.addEventListener('mouseup', endDrag);
+    list.addEventListener('mousemove', drag);
+    
+    // Touch events
+    list.addEventListener('touchstart', startDrag);
+    list.addEventListener('touchend', endDrag);
+    list.addEventListener('touchmove', drag);
+    
+    function startDrag(e) {
+        isDown = true;
+        startX = e.type === 'mousedown' ? e.pageX : e.touches[0].pageX;
+        scrollLeft = list.scrollLeft;
+    }
+    
+    function endDrag() {
+        isDown = false;
+    }
+    
+    function drag(e) {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.type === 'mousemove' ? e.pageX : e.touches[0].pageX;
+        const walk = (x - startX) * 2; // Adjust scroll speed here
+        list.scrollLeft = scrollLeft - walk;
+    }
+})()
+
+window.addEventListener('load', function() {
+    var overlay = document.querySelector('.loading-screen');
+    var body = this.document.querySelector('body')
+    body.style.overflowY = 'auto'
+    overlay.style.display = 'none';
+  });
